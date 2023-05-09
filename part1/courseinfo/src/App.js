7aeeb90a708fc83cb135fddc986818b0839747e2
Zarak Ansari <zarak.ansari@yahoo.com>
2023-05-09 06:54:40 +0500

    completed part 1a

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,45 @@
+
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
+const Header = (props) => {
+  return (<h1>{props.course}</h1>)
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (<p>{props.part} {props.exercise}</p>)
+}
+
+const Total = () => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
