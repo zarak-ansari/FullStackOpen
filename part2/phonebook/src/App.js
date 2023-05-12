bb7ff3c58266dd54875fa69cfc6b981abb59208c
Zarak Ansari <zarak.ansari@yahoo.com>
2023-05-12 08:31:33 +0500

    2-13 in progress

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+// import axios from 'axios'
+import { getAll, create } from './components/personsService'
 
 const PersonForm = ({ name, changeName, newNumber, changeNumber, addNewName }) => {
   return (
@@ -24,20 +26,23 @@ const PersonForm = ({ name, changeName, newNumber, changeNumber, addNewName }) =
 
 const Filter = ({ searchInput, setSearchInput }) => <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
 
-const Person = ({ person }) => <p key={person.id}>{person.name} {person.number}</p>
+const Person = ({ person }) => <p key={person.id}>{person.name} {person.number} <button>delete</button></p>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchInput, setSearchInput] = useState('')
 
+  useEffect(() => {
+    const initialPersons = getAll()
+    setPersons(initialPersons)
+  }
+    , []
+  )
 
   const addNewName = (event) => {
     event.preventDefault()
@@ -50,12 +55,13 @@ const App = () => {
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: (persons.length + 1)
       }
-      setPersons(persons.concat(newPerson))
-      console.log(personsToShow)
+
+      const personCreatedOnServer = create(newPerson)
+      setPersons(persons.concat(personCreatedOnServer))
     }
   }
+
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(searchInput.toLowerCase()))
 
